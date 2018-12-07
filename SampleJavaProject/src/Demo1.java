405bfca7db72c5b0ed130f817b3a1ef5963c819d
HOME <HOME@DESKTOP-SH48H1L>
2018-12-07 07:19:43 +0530

    VRK : SampleJavaProject --> Demo1 java file added

---
+++ SampleJavaProject/src/Demo1.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author Veerraju Kakarla
+ *
+ */
+public class Demo1 {
+
+	public static void main(String[] args) {
+        System.out.println("OM Namo Narayanaya : Welocome To Java World");
+	}
+
+}
