668eaff52afcecc8e0b731619b1ed6937fc1680e
veerrajukakarla <40323661+veerrajukakarla@users.noreply.github.com>
2018-12-07 07:22:12 +0530

    Update Demo1.java

--- SampleJavaProject/src/Demo1.java
+++ SampleJavaProject/src/Demo1.java
@@ -7,6 +7,8 @@ public class Demo1 {
 
 	public static void main(String[] args) {
         System.out.println("OM Namo Narayanaya : Welocome To Java World");
+		System.out.println("OM Namo Narayanaya : Welocome To Git Hub World");
+		
 	}
 
 }
