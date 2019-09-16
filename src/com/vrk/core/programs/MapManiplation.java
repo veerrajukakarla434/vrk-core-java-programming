c6fcd3b9e0ae1a61b7d768e8d3cba1ba72e4505d
Veerraju Kakarla <VKakarla@delta.org>
2019-09-16 18:21:58 +0530

    Added Map logic

--- src/com/vrk/core/programs/MapManiplation.java
+++ src/com/vrk/core/programs/MapManiplation.java
@@ -1,33 +1,29 @@
 package com.vrk.core.programs;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MapManiplation {
 
 	public static void main(String[] args) {
 		
-		Map<Integer, String>map= new HashMap<>();
-		map.put(100, "veera");
-		map.put(101, "raju");
-		map.put(103, "praveen");
-		map.put(104, "veera");
-		map.put(105, "praveen");
-		map.put(106, "veera");
-		map.put(107, "veera");
-		map.put(108, "veera");
-		map.put(109, "satya");
+		Map<String, String>map= new HashMap<>();
+		map.put("100", "veera");
+		map.put("101", "raju");
+		map.put("103", "praveen");
+		map.put("104", "veera");
+		map.put("105", "praveen");
+		map.put("106", "veera");
+		map.put("107", "veera");
+		map.put("108", "veera");
+		map.put("109", "satya");
 		
 		
-		Map<String, List<Integer>>map1= new HashMap<>();
+		Map<String, String>map1= new HashMap<>();
 		
-		List<Integer> list=new ArrayList<>();
 		
-		for(Map.Entry<Integer, String> entry : map.entrySet()) {
-			map1.containsKey(entry.getValue());
-			list.add(entry.getKey());
+		for(Map.Entry<String, String> entry : map.entrySet()) {
+			map1.put(entry.getValue(), map1.get(entry.getValue())==null ? entry.getKey() : map1.get(entry.getValue())+","+entry.getKey());
 		}
 		System.out.println(map1);
        
